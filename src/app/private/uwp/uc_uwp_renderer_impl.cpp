5bec87ff9584f0bc0a34c3d0f5528178f2173a5f
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2020-02-23 22:53:47 +0200

    fixed issue with showing world twice

--- src/app/private/uwp/uc_uwp_renderer_impl.cpp
+++ src/app/private/uwp/uc_uwp_renderer_impl.cpp
@@ -80,7 +80,7 @@ namespace uc
                 c0.m_geometry = m_geometry_allocator.get();
                 c0.m_resources = &m_resources;		
                 m_render_world_manager = std::make_unique<gxu::render_world_manager>(&c0);
-                m_render_world_manager->show_world(uc::uwp::gxu::world_id::non_linear_moment_shadow_maps_64);
+                //m_render_world_manager->show_world(uc::uwp::gxu::world_id::non_linear_moment_shadow_maps_64);
 
                 
                 {
